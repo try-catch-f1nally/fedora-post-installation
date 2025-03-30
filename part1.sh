28bca0c3d9823866d1a58a6e6cd70a5941692dca
try_catch_finally <finally.catch.try@gmail.com>
2025-03-30 17:46:31 +0300

    Fix part1.sh

--- part1.sh
+++ part1.sh
@@ -1,7 +1,6 @@
 set -e
 PROJECT_DIR=$(dirname $0)
 
-
 echo "
 max_parallel_downloads=10
 defaultyes=True
@@ -32,17 +31,23 @@ sudo dnf upgrade -y
 sudo dnf install -y vim zsh xclip google-chrome-stable vlc kolourpaint
 flatpak install -y flathub org.telegram.desktop com.github.ryonakano.reco
 
-
-# SET UP TERMINAL
-# https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#getting-started
-mkdir ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts
 cp -r $PROJECT_DIR/fonts/* ~/.local/share/fonts
-cp $PROJECT_DIR/inis/gnome-with-white-font.palette ~/.local/share/org.gnome.Ptyxis/palettes/gnome-with-white-font.palette
+cp $PROJECT_DIR/inis/gnome-with-white-font.palette ~/.local/share/org.gnome.Ptyxis/palettes/gnome-white-font.palette
 fc-cache
 gsettings set org.gnome.Ptyxis use-system-font false
-dconf write /org/gnome/Ptyxis/Profiles/$(gsettings get org.gnome.Ptyxis profile-uuids | tr -d "[]',")/palette "'gnome-with-white-font'"
+dconf write /org/gnome/Ptyxis/Profiles/$(gsettings get org.gnome.Ptyxis profile-uuids | tr -d "[]',")/palette "'gnome-white-font'"
+
+xclip -selection clipboard <<< 'export SHELL=/usr/bin/zsh && 
+  gsettings set org.gnome.Ptyxis font-name "MesloLGS NF 11" && 
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+
+echo 'Part1 almost done. Now:
+  - enter your password to change user shell;
+  - execute the command from clipboard;
+  - run part2.sh to proceed.
+'
+
 chsh -s /usr/bin/zsh
-export SHELL=/usr/bin/zsh
-echo Now execute the command below and run part2.sh
-echo 'gsettings set org.gnome.Ptyxis font-name "MesloLGS NF 11" && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+
 

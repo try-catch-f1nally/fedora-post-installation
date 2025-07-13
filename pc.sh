bf0381bda0195eb42569d47619eae1d33dd0440b
Mykyta K <mykyta.k.as@agiliumlabs.com>
2025-07-13 22:13:36 +0300

    Fix systemd unit names

--- pc.sh
+++ pc.sh
@@ -16,9 +16,9 @@ sudo chmod +x /usr/local/bin/adjust_display_brightness_on_login.sh
 mkdir -p ~/.config/systemd/user
 cp $PROJECT_DIR/systemd-units/adjust_rgb_brightness_by_time.service             ~/.config/systemd/user/adjust_rgb_brightness_by_time.service
 cp $PROJECT_DIR/systemd-units/adjust_rgb_brightness_by_time.timer               ~/.config/systemd/user/adjust_rgb_brightness_by_time.timer
-cp $PROJECT_DIR/systemd-units/user/adjust_display_brightness_by_time.service    ~/.config/systemd/user/adjust_display_brightness_by_time.service
+cp $PROJECT_DIR/systemd-units/adjust_display_brightness_by_time.service         ~/.config/systemd/user/adjust_display_brightness_by_time.service
 cp $PROJECT_DIR/systemd-units/adjust_display_brightness_by_time.timer           ~/.config/systemd/user/adjust_display_brightness_by_time.timer
-cp $PROJECT_DIR/systemd-units/adjust_display_brightness_on_login.service        ~/.config/systemd/useradjust_display_brightness_on_login.service
+cp $PROJECT_DIR/systemd-units/adjust_display_brightness_on_login.service        ~/.config/systemd/user/adjust_display_brightness_on_login.service
 
 systemctl --user enable --now adjust_rgb_brightness_by_time.service
 systemctl --user enable --now adjust_rgb_brightness_by_time.timer
@@ -27,4 +27,4 @@ systemctl --user enable --now adjust_display_brightness_by_time.timer
 systemctl --user enable --now adjust_display_brightness_on_login.service
 
 echo All done.
-echo Don't forget to manually set refresh rate for monitors in Settings.
+echo Don\'t forget to manually set refresh rate for monitors in Settings.
